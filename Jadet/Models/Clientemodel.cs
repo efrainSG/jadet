85f676b4837765419d291517b3af2528c7a0eb64
Efrain <efrain@sernasis.com>
2020-07-09 08:01:13 -0500

    Actualización
    Modelo de cliente en front e imágenes.

---
+++ Jadet/Models/Clientemodel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.PeerResolvers;
+using System.Web;
+
+namespace Jadet.Models
+{
+    public class clientenmodel
+    {
+        public string usuario { get; set; }
+        public string password { get; set; }
+        public string Nombre { get; set; }
+        public int ErrorNumero { get; set; }
+        public string ErrorMensaje { get; set; }
+    }
+    public class listaclientesmodel
+    {
+        public List<clientenmodel> Items { get; set; }
+        public listaclientesmodel()
+        {
+            Items = new List<clientenmodel>();
+        }
+    }
+}
