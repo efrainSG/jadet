c884e20f24f643e79e04c3e1b6a5458359958193
Efrain <efrain@sernasis.com>
2020-07-09 10:39:48 -0500

    Actualización
    Modelos y vistas

--- Jadet/Controllers/AdministradorController.cs
+++ Jadet/Controllers/AdministradorController.cs
@@ -23,7 +23,113 @@ namespace Jadet.Controllers
 
         public ActionResult Productos()
         {
-            return View();
+            if (Session["usuario"] == null || (Session["usuario"] as loginmodel).usuario != "Root")
+            {
+                Session.Clear();
+                return RedirectToAction("Index", "Home");
+            }
+            listaproductosmodel productos = new listaproductosmodel();
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/103373701_745367459536215_6683955577034642454_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/106726574_619939592236905_8451229284513577533_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/106925028_276980433358784_3564958967851603976_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107100168_575722183139154_7671637487869851358_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107343952_285192662700377_4387108442096812264_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107485999_761173144656157_7653469568639381490_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107700091_605326170114874_5692240400939541068_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107728691_741771619931603_148549030520414310_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107761698_275912170409017_4174448911657339959_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            productos.Items.Add(new productomodel
+            {
+                Sku = "A00001",
+                Nombre = "Producto 1",
+                Descripcion = "Producto uno",
+                Existencias = 10,
+                RutaImagen = "../Content/imgs/107856000_702503103934248_1656178707960925460_n.jpg",
+                ErrorNumero = 0,
+                ErrorMensaje = string.Empty
+            });
+            return View(productos);
         }
 
         public ActionResult Guias()
@@ -33,8 +139,14 @@ namespace Jadet.Controllers
 
         public ActionResult Clientes()
         {
+            if (Session["usuario"] == null || (Session["usuario"] as loginmodel).usuario != "Root")
+            {
+                Session.Clear();
+                return RedirectToAction("Index", "Home");
+            }
+
             listaclientesmodel clientes = new listaclientesmodel();
-            clientes.Items.Add(new clientenmodel
+            clientes.Items.Add(new clientemodel
             {
                 ErrorMensaje = string.Empty,
                 ErrorNumero = 0,
@@ -42,7 +154,7 @@ namespace Jadet.Controllers
                 usuario = "user1",
                 password = string.Empty
             });
-            clientes.Items.Add(new clientenmodel
+            clientes.Items.Add(new clientemodel
             {
                 ErrorMensaje = string.Empty,
                 ErrorNumero = 0,
@@ -50,7 +162,7 @@ namespace Jadet.Controllers
                 usuario = "user2",
                 password = string.Empty
             });
-            clientes.Items.Add(new clientenmodel
+            clientes.Items.Add(new clientemodel
             {
                 ErrorMensaje = string.Empty,
                 ErrorNumero = 0,
@@ -58,7 +170,7 @@ namespace Jadet.Controllers
                 usuario = "user3",
                 password = string.Empty
             });
-            clientes.Items.Add(new clientenmodel
+            clientes.Items.Add(new clientemodel
             {
                 ErrorMensaje = string.Empty,
                 ErrorNumero = 0,
@@ -66,7 +178,7 @@ namespace Jadet.Controllers
                 usuario = "user4",
                 password = string.Empty
             });
-            clientes.Items.Add(new clientenmodel
+            clientes.Items.Add(new clientemodel
             {
                 ErrorMensaje = string.Empty,
                 ErrorNumero = 0,
