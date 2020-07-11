6cee5666ba85e3db198843d473c8d4585577d789
Efrain <efrain@sernasis.com>
2020-07-10 19:54:15 -0500

    Esquemas
    SS. PP. y tablas.

---
+++ SernaSistemas.Jadet.DataAccess/Esquema_2.sql
@@ -0,0 +1,24 @@
+﻿create procedure Administracion.guardarEstatus
+	@Id int = 0,
+	@Nombre varchar(50),
+	@IdTipoEstatus int
+as
+begin
+	declare @identity int
+
+	if exists (select 1 from Administracion.Estatus (nolock) where Id = @Id)
+	begin
+		update Administracion.Estatus
+		set Nombre = @Nombre, IdTipoEstatus = @IdTipoEstatus
+		where Id = @Id
+		set @identity = @Id
+	end
+	else
+	begin
+		insert into Administracion.Estatus(Nombre, IdTipoEstatus)
+		values (@Nombre, @IdTipoEstatus)
+		select @identity = SCOPE_IDENTITY()
+	end
+
+	select @identity Id, @Nombre Nombre, @IdTipoEstatus IdTipoEstatus
+end;
