799b049a0191b5eb00c0ae441d3a9e3ba2d2a920
Efrain <efrain_serna@hotmail.com>
2020-08-11 23:57:51 -0500

    Ajuste
    Se agregan inicializadores de listas.

--- SernaSistemas.Jadet.WCF/Modelos/CarritoModelos.cs
+++ SernaSistemas.Jadet.WCF/Modelos/CarritoModelos.cs
@@ -25,11 +25,17 @@ namespace SernaSistemas.Jadet.WCF.Modelos {
     [DataContract]
     public class CarritoResponse : BaseResponse {
         public List<ItemCarritoResponse> Items { get; set; }
+        public CarritoResponse() {
+            Items = new List<ItemCarritoResponse>();
+        }
     }
 
     [DataContract]
     public class ColeccionCarritoResponse : BaseResponse {
         [DataMember]
         public List<CarritoResponse> Items { get; set; }
+        public ColeccionCarritoResponse() {
+            Items = new List<CarritoResponse>();
+        }
     }
 }
